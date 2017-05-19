af110f835ccaf4151e948589e72b1b215ff9180b
relentlessOne <b.iwaszkie@gmail.com>
2017-05-19 11:34:32 +0200

    added pause menu

--- App4/js/main.js
+++ App4/js/main.js
@@ -6,6 +6,9 @@ var mapArr;
 var arrayForAStar;
 var easystar;
 
+var w = 1024;
+var h = 760;
+
 function preload() {
 
     var csvArr = new csvToArr();
@@ -67,6 +70,9 @@ var bullets;
 var fireRate = 150;
 var nextFire = 0;
 
+
+
+
 function create() {
     game.physics.startSystem(Phaser.Physics.P2JS);
 
@@ -121,12 +127,68 @@ function create() {
     myHealthBar = new HealthBar(game, { x: 80, y: 50, width: 120 });
     myHealthBar.setFixedToCamera(true);
 
-    manaBar = new HealthBar(game, { x: 580, y: 50, width: 120 });
+    manaBar = new HealthBar(game, { x: 850, y: 50, width: 120 });
     manaBar.setFixedToCamera(true);
 
-    expBar = new HealthBar(game, { x:380, y: 50, width: 120 });
+    expBar = new HealthBar(game, { x:450, y: 50, width: 120 });
     expBar.setFixedToCamera(true);
     expBar.setPercent(0);
+
+
+
+
+    // Create a label to use as a button
+    pause_label = game.add.text(w - 100, 20, 'Pause', { font: '24px Arial', fill: '#fff' });
+    pause_label.inputEnabled = true;
+    pause_label.events.onInputUp.add(function () {
+        // When the paus button is pressed, we pause the game
+        game.paused = true;
+
+        // Then add the menu
+        menu = game.add.sprite(w / 2, h / 2, 'menu');
+        menu.anchor.setTo(0.5, 0.5);
+
+        // And a label to illustrate which menu item was chosen. (This is not necessary)
+        choiseLabel = game.add.text(w / 2, h - 150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
+        choiseLabel.anchor.setTo(0.5, 0.5);
+    });
+
+
+    game.input.onDown.add(unpause, self);
+
+    // And finally the method that handels the pause menu
+    function unpause(event) {
+        // Only act if paused
+        if (game.paused) {
+            // Calculate the corners of the menu
+            var x1 = w / 2 - 270 / 2, x2 = w / 2 + 270 / 2,
+                y1 = h / 2 - 180 / 2, y2 = h / 2 + 180 / 2;
+
+            // Check if the click was inside the menu
+            if (event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2) {
+                // The choicemap is an array that will help us see which item was clicked
+                var choisemap = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+                // Get menu local coordinates for the click
+                var x = event.x - x1,
+                    y = event.y - y1;
+
+                // Calculate the choice 
+                var choise = Math.floor(x / 90) + 3 * Math.floor(y / 90);
+
+                // Display the choice
+                choiseLabel.text = 'You chose menu item: ' + choisemap[choise];
+            }
+            else {
+                // Remove the menu and the label
+                menu.destroy();
+                choiseLabel.destroy();
+
+                // Unpause the game
+                game.paused = false;
+            }
+        }
+    };
 
 }
 
